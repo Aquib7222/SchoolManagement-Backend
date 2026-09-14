371b825804773c0a96ff6f9c37f96fd0c2af4466
Aquib7222 <aquibshahzada@gmail.com>
2026-09-14 11:28:59 +0530

    update backup scheduler

--- src/main/java/com/schoolmanagement/schoolmanagementwebsite/backup/BackupScheduler.java
+++ src/main/java/com/schoolmanagement/schoolmanagementwebsite/backup/BackupScheduler.java
@@ -19,10 +19,14 @@ public class BackupScheduler {
      *
      * Spring server timezone is explicitly set to Asia/Kolkata.
      */
+    // @Scheduled(
+    //         cron = "0 0 2 * * *",
+    //         zone = "Asia/Kolkata"
+    // )
     @Scheduled(
-            cron = "0 0 2 * * *",
-            zone = "Asia/Kolkata"
-    )
+        cron = "0 */5 * * * *",
+        zone = "Asia/Kolkata"
+)
     public void dailyBackup() {
 
         System.out.println(
